af83e35fc4b85c32fd9fdfa2aa53424f73afeae9
AlexandreSarmento <48884424+AlexandreSarmento@users.noreply.github.com>
2023-08-16 09:28:55 -0300

    Update cells.py

--- cells.py
+++ cells.py
@@ -5,97 +5,87 @@ This script have the mother class Population and the daugther class Skmel and Ha
 
 import neighbors
 import random
-import basin2attractor
+#import basin2attractor
 import models
 import settingInput
 
 class Population():
     
-    def __init__(self,index,nghVertexDict):
+    def __init__(self,index,nghVertexDict,pmax):
         
         
         self.index = index # index: the address in term of row and col of the cell (tuple)
+        self.nghAddress = nghVertexDict[self.index] # nghAddress: list of tuples. here we get the 
+        #values of a dicitionay as a list of tuples
+        self.CC = settingInput.K ## self.CC: carrying capacity (int)
         
-        self.nghAddress = nghVertexDict[self.index] # nghAddress: list of tuples. here we get the values of a dicitionay as a list of tuples
-     
-    def updateBooleanNetwork(self,agents,i):
         
+    def updateBooleanNetwork(self,agents): #probMigProlS,probMigProlH,i
         
         [listOfEmptyIndex,numberOfEmptyIndex,numberOfSkmel,numberOfHacat] = neighbors.updateNeighbors(self,agents)
-        [migration,proliferation] = basin2attractor.updateMicroEnvironment(numberOfEmptyIndex,numberOfSkmel,numberOfHacat)
-        plog = models.updateProliferationProbability(self.lambA,i)
-        r1 = random.uniform(0,1)
-        r2 = random.uniform(0,1)
-        self.proliferate = (numberOfEmptyIndex >= 1) and ((plog > r1) or proliferation)  
-        self.migrate = (numberOfEmptyIndex >= 1) and migration 
-        self.survive = not self.proliferate and self.migrate and ((1 - plog) > r2) 
-        self.death = not self.proliferate and not self.migrate and not self.survive
+        [migration,proliferation] = models.updateMicroEnvironment(numberOfEmptyIndex,numberOfSkmel,numberOfHacat)
+        pprol = 1 - (len(agents)/self.CC)
+        if self.cellsID == 1:
+            
+            self.death = numberOfEmptyIndex == 0 and self.rhoMax <= 0
+            self.proliferate = (numberOfEmptyIndex >= 1) and (proliferation or pprol > random.uniform(0,1)) and not self.death
+            self.migrate = (numberOfEmptyIndex >= 1) and migration and not self.death
+            self.survive = not self.death and not self.proliferate and not self.migrate
+        else:
+            #self.death = self.pdie > random.uniform(0,1)
+            self.survive = self.rhoMax <= 0
+            self.proliferate = (numberOfEmptyIndex >= 1) and (proliferation or pprol > random.uniform(0,1)) and not self.survive           
+            self.migrate = (numberOfEmptyIndex >= 1) and migration and not self.survive
+            self.death = not self.survive and not self.proliferate and not self.migrate
+            
         self.listOfEmptyIndex = listOfEmptyIndex
-    
+        
     def updateTumorGrowthKinetics(self,agents,nghVertexDict):
         
         if self.proliferate:
-            if agents[self.index].id_number == 1:        
+            if agents[self.index].cellsID == 1:        
                 emptyIndex = random.choice(self.listOfEmptyIndex)
-                agents[emptyIndex] = Skmel(emptyIndex,nghVertexDict)
+                pmax = self.rhoMax - 1
+                agents[self.index] = Skmel(self.index,nghVertexDict,pmax)
+                agents[emptyIndex] = Skmel(emptyIndex,nghVertexDict,pmax)
             else:
                 emptyIndex = random.choice(self.listOfEmptyIndex)
-                agents[emptyIndex] = Hacat(emptyIndex,nghVertexDict)
+                pmax = self.rhoMax - 1
+                agents[self.index] = Hacat(self.index,nghVertexDict,pmax)
+                agents[emptyIndex] = Hacat(emptyIndex,nghVertexDict,pmax)
+                
+                
         elif self.migrate:
-                if agents[self.index].id_number == 1:
+                if agents[self.index].cellsID == 1:
                     del agents[self.index]
                     emptyIndex = random.choice(self.listOfEmptyIndex)
-                    agents[emptyIndex] = Skmel(emptyIndex,nghVertexDict)
+                    pmax = self.rhoMax
+                    agents[emptyIndex] = Skmel(emptyIndex,nghVertexDict,pmax)
                 else:
                     del agents[self.index]
                     emptyIndex = random.choice(self.listOfEmptyIndex)
-                    agents[emptyIndex] = Hacat(emptyIndex,nghVertexDict)
+                    pmax = self.rhoMax 
+                    agents[emptyIndex] = Hacat(emptyIndex,nghVertexDict,pmax)
         else:
             if self.survive:
-                pass
+                pass                
             else:
                 del agents[self.index]
             
+                
 
 class Skmel(Population):
           
-    def __init__(self,index,nghVertexDict):
+    def __init__(self,index,nghVertexDict,pmax):
         
-        super().__init__(index,nghVertexDict)
-        self.id_number = 1
-        self.lambA = settingInput.lamb[0]#0.1
-         
+        super().__init__(index,nghVertexDict,pmax)
+        self.cellsID = 1
+        self.rhoMax = pmax
+
 class Hacat(Population):
           
-    def __init__(self,index,nghVertexDict):
+    def __init__(self,index,nghVertexDict,pmax):
         
-        super().__init__(index,nghVertexDict)
-        self.id_number = 2
-        self.lambA = settingInput.lamb[1]#0.17
-        
-        
-
-
-# if self.proliferate:
-    
-#     if agents[self.index].id_number == 1:
-#         emptyIndex = random.choice(self.listOfEmptyIndex)
-#         agents[emptyIndex] = Skmel(emptyIndex,nghVertexDict)
-#     else:
-#         emptyIndex = random.choice(self.listOfEmptyIndex)
-#         agents[emptyIndex] = Hacat(emptyIndex,nghVertexDict)
-# else:
-#     if self.migrate:
-#         if agents[self.index].id_number == 1:
-#             del agents[self.index]
-#             emptyIndex = random.choice(self.listOfEmptyIndex)
-#             agents[emptyIndex] = Skmel(emptyIndex,nghVertexDict)
-#         else:
-#             del agents[self.index]
-#             emptyIndex = random.choice(self.listOfEmptyIndex)
-#             agents[emptyIndex] = Hacat(emptyIndex,nghVertexDict)
-#     else:
-#         if self.death:
-#             del agents[self.index]
-#         else:
-#             pass
+        super().__init__(index,nghVertexDict,pmax)
+        self.cellsID = 2
+        self.rhoMax = pmax
