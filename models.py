308804bcaa860a2198e90758fe18817ad0dc8958
AlexandreSarmento <48884424+AlexandreSarmento@users.noreply.github.com>
2023-08-16 09:28:03 -0300

    Update models.py

--- models.py
+++ models.py
@@ -9,6 +9,7 @@ The function getCorrelation return the spearman correlation among tumor growth d
 
 import numpy as np
 from scipy.stats import pearsonr,spearmanr
+import math
 
 def Velhurst(t,CC,rho,tau):
     
@@ -28,35 +29,72 @@ def Velhurst(t,CC,rho,tau):
     return CC/(1 + np.exp(-rho*(t-tau))) 
 
 
-def updateProliferationProbability(lamb,i):
+def updateKineticsProbs(listOfCells):
+    
     '''
     Input
-        lamb: parameter lambda
-        i: arbitrary unit of time
+        listOfCells: list of object Skmel/Hacat instantiated at a given iteration
     Processing
-        calculate the probability of the population growth be in log phase as a function of time
-
+        Here we count the amount of cell which proliferate, migrate, died and survive
     Output
-        probability of the population growth be in log phase
+        Ps: skmel probability of proliferation
+        Ds: skmel probability of death
+        Ms: skmel probability of migration
+        Ss: skmel probability of survive
+        Ph: hacat probability of proliferation
+        Dh: hacat probability of death
+        Mh: hacat probability of migration
+        Sh: hacat probability of survive
+    
+    '''
+    skmelProlif = len([p for p in listOfCells if p.cellsID == 1 and p.proliferate == True])
+    skmelDeath = len([p for p in listOfCells if p.cellsID == 1 and p.death == True])
+    skmelMig = len([p for p in listOfCells if p.cellsID == 1 and p.migrate == True])
+    skmelSurv = len([p for p in listOfCells if p.cellsID == 1 and p.survive == True])
+    hacatProlif = len([p for p in listOfCells if p.cellsID == 2 and p.proliferate == True])
+    hacatDeath = len([p for p in listOfCells if p.cellsID == 2 and p.death == True])
+    hacatMig = len([p for p in listOfCells if p.cellsID == 2 and p.migrate == True])
+    hacatSurv = len([p for p in listOfCells if p.cellsID == 2 and p.survive == True])
+    
+    Ps = skmelProlif/(skmelProlif+skmelDeath+skmelMig+skmelSurv)
+    Ds = skmelDeath/(skmelProlif+skmelDeath+skmelMig+skmelSurv)
+    Ms = skmelMig/(skmelProlif+skmelDeath+skmelMig+skmelSurv)
+    Ss = skmelSurv/(skmelProlif+skmelDeath+skmelMig+skmelSurv)
+    Ph = hacatProlif/(hacatProlif+hacatDeath+hacatMig+hacatSurv)
+    Dh = hacatDeath/(hacatProlif+hacatDeath+hacatMig+hacatSurv)
+    Mh = hacatMig/(hacatProlif+hacatDeath+hacatMig+hacatSurv)
+    Sh = hacatSurv/(hacatProlif+hacatDeath+hacatMig+hacatSurv)
+    
+    
+    return Ps,Ds,Ms,Ss,Ph,Dh,Mh,Sh
+    
+def updateMicroEnvironment(SPC,MEL,KCT):
+    
+    '''
+    Input
+        SPC: number of empty neighbors (int)
+        MEL: number of vertex occupied by skmel (int)
+        KCT: number of vertex occupied by hacat (int)
+    Processing
+        This function update the boolean expressions that describes the tolerance to high density
+    Output
+        migration: condition to cell migrate (boolean)
+        proliferation: condition to cell proliferate (boolean)
+        
     '''
     
-    return np.exp(-lamb*i)
-
-# def updateProliferationProbability(lamb,i):
+    migration = (MEL <= SPC) or (MEL <= KCT)
+    proliferation = (MEL >= SPC) or (MEL >= KCT)
+      
+    return migration, proliferation
     
-#     return np.exp(-lamb*i)
-
-
-def getCorrelation(meanDict):
+def getCorrelation(dataSimulation):
     
-    skmelPS, _ = spearmanr(meanDict['prolS'],meanDict['survS'])
-    skmelPM, _ = spearmanr(meanDict['prolS'],meanDict['migS'])
-    skmelPD, _ = spearmanr(meanDict['prolS'],meanDict['deathS'])
-    hacatPS, _ = spearmanr(meanDict['prolH'],meanDict['survH'])
-    hacatPM, _ = spearmanr(meanDict['prolH'],meanDict['migH'])
-    hacatPD, _ = spearmanr(meanDict['prolH'],meanDict['deathH'])
+    skmelPS, _ = pearsonr(dataSimulation['survS'],dataSimulation['prolS'])
+    skmelPM, _ = pearsonr(dataSimulation['migS'],dataSimulation['prolS'])
+    skmelPD, _ = pearsonr(dataSimulation['deathS'],dataSimulation['prolS'])
+    hacatPS, _ = pearsonr(dataSimulation['survH'],dataSimulation['prolH'])
+    hacatPM, _ = pearsonr(dataSimulation['migH'],dataSimulation['prolH'])
+    hacatPD, _ = pearsonr(dataSimulation['deathH'],dataSimulation['prolH'])
     
     return skmelPS,skmelPM,skmelPD,hacatPS,hacatPM,hacatPD
-
-
-
